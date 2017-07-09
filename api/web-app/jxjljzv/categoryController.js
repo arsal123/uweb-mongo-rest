bd77528c41f4d4fb8ef70a9205d48a60829e4b12
Arsalan <arsalan.siddiqui@philips.com>
2017-07-09 12:40:30 -0400

    fixing category interface

--- api/web-app/jxjljzv/categoryController.js
+++ api/web-app/jxjljzv/categoryController.js
@@ -2,6 +2,20 @@
     angular.module('jewelCrud', [])
         .controller('categoryController', ['$scope', '$http', '$log', function ($scope, $http, $log) {
 
+            $scope.data = {};
+
+            let getBaseCategories = () => {
+                $http({
+                    method: 'GET',
+                    url: '/baseCategory'
+                })
+                    .then(function (res) {
+                        console.log('Got baseCategory response: ' + JSON.stringify(res.data));
+                        $scope.data.baseCategories = res.data;
+                    }, function (err) {
+                        $log.err('Base Categories rest call failed');
+                    });
+            }
 
             let getCategories = () => {
                 $http({
@@ -10,11 +24,12 @@
                 })
                     .then(function (res) {
                         console.log('Got response: ' + JSON.stringify(res.data));
-                        $scope.categories = res.data;
+                        $scope.data.categories = res.data;
                     }, function (err) {
 
                     });
             }
+
 
             let init = () => {
                 $scope.cat = {};
@@ -22,7 +37,19 @@
                 $scope.updateOperation = false;
 
             }
+            let catValidation = function(cat){
+                if (!cat.name || !cat.base_category_id) {
+                    throw new 'Invalid name or baseCategoryId';
+                }       
+            }
             $scope.addCategory = () => {
+                $log.debug("baseCat val: " + $scope.cat.base_category_id);
+                // $log.debug("baseCat state: " + $scope.cat.baseCategoryId.$valid);
+                // if (!$scope.cat.name || !$scope.cat.base_category_id) {
+                //     throw new 'Invalid name or baseCategoryId';
+                // }
+                catValidation($scope.cat);
+
                 $http({
                     method: 'POST',
                     url: '/category',
@@ -45,6 +72,7 @@
             }
 
             $scope.updateCategory = (category) => {
+                catValidation(category);
                 $log.debug('Pre Update Call: ' + JSON.stringify(category));
                 const url1 = '/category/' + category._id;
                 $log.debug('Calling url:' + url1);
@@ -75,6 +103,7 @@
 
             // Start of command execution
             init();
+            getBaseCategories();
             getCategories();
 
         }])
